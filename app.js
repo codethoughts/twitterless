64e3cdd646076a7a7234ea6163855231e75f732c
Dmitry Sirotkin <sirotkin.dmitry@yahoo.com>
2017-01-18 14:28:57 +0800

    Unsubscribe from unfollowers with given interval

--- app.js
+++ app.js
@@ -6,23 +6,9 @@ var app = express();
 var Twitter = require("twitter");
 var MY_USERNAME = "mutablemind";
 var _ = require("lodash");
-
+const request = require('request-promise');
 var request_ip = require("request-ip");
 var dbip = require("dbip");
-
-app.set("port", (process.env.PORT || 5000));
-
-app.get("/", function (req, res) {
-	var ip = request_ip.getClientIp(req);
-	dbip(ip).then(ip_info => {
-		console.log({"event": "Request / redirected", "ip_info": ip_info});
-	});
-	res.redirect("http://twitter.com/mutablemind");
-});
-
-app.listen(app.get("port"), function () {
-	console.log("App is running on " + app.get("port") + " port");
-});
 
 var client = new Twitter({
 	consumer_key: process.env.CONSUMER_KEY,
@@ -37,29 +23,69 @@ const isTweet = _.conforms({
 	text: _.isString
 })
 
+
+app.set("port", (process.env.PORT || 5000));
+
+app.get("/", function (req, res) {
+	var ip = request_ip.getClientIp(req);
+	dbip(ip).then(ip_info => {
+		console.log({"event": "Request / redirected", "ip_info": ip_info});
+	});
+	res.redirect("http://twitter.com/"+MY_USERNAME);
+});
+
+app.listen(app.get("port"), function () {
+	console.log("App is running on " + app.get("port") + " port");
+	setInterval(unsubscribeFromUnfollowers, randomDelayTimeMs(200,500));
+});
+
+var getUnfollowers = new Promise((resolve,reject) => {
+	// get sync list of unsubscribed users
+	client.get("followers/list", {screen_name: MY_USERNAME},
+			(err, data, res) => {
+				const friends = data.users.filter((user) => { return !user.following });
+				err ? reject(err) : resolve(friends);
+			}
+		);
+});
+
+function unsubscribeFromUnfollowers() {
+	getUnfollowers.then((users) => {
+		users.forEach((user)=>{
+			client.post("friendships/destroy", { screen_name: user.screen_name },
+			(err, data, res) => {
+				if (!err) console.log({"event":"DESTROY CONNECTION "+user.screen_name});
+				else console.error(err);
+			});
+		});
+	}).catch((err) => {
+		console.error(err)
+	});
+}
+
 // User stream is used to handle primary action triggered by the user
 var user_stream = client.stream("user");
 // Handle follow event
 user_stream.on("follow", function(event) {
 	var username = event.source.screen_name;
 	if (username == MY_USERNAME) return;
-	var delayTimeForDirect = randomDelayTimeMs(23,26);
+	var delayTime = randomDelayTimeMs(40,120);
 	setTimeout(function(){
-		client.post("direct_messages/new", 
+		client.post("direct_messages/new",
 			{
-				screen_name: username, 
+				screen_name: username,
 				text: "Thanks for following me on twitter :3 Keep in touch on dieser.me!"
 			}, function(error, data, response) {
 				if (!error) console.log({"event":"SEND DIRECT TO "+username});
 				else console.error(error);
 			});
-	},delayTimeForDirect);
+	},delayTime);
 
+	// Avoid bot or fake account following
 	var user_followers = event.source.followers_count;
 	var user_followings = event.source.friends_count;
 	var ratio = user_followers / user_followings;
 	if (ratio < 1.0) return;
-	var delayTimeForFollow = randomDelayTimeMs(26,30);
 	setTimeout(function(){
 		client.post("friendships/create", {
 			screen_name: username,
@@ -68,7 +94,7 @@ user_stream.on("follow", function(event) {
 			if (!error) console.log({"event":"FOLLOW "+username});
 			else console.error(error);
 		});
-	},delayTimeForFollow);
+	},delayTime);
 });
 
 user_stream.on("error", function(error) {
@@ -78,12 +104,12 @@ user_stream.on("error", function(error) {
 var TECH_STRINGS = require("./keywords.js").tech();
 
 // Statuses stream is used to filter twits  according to the given tracking request
-var tech_stream = client.stream("statuses/filter", 
+var tech_stream = client.stream("statuses/filter",
 		{
-			track: "startup,football,business,technology", 
+			track: "technology",
 			language: "en",
 			filter_level: "medium"
-		}, 
+		},
 		function(stream) {
 			stream.on("data", function(event) {
 				var status_id = event.id;
@@ -95,7 +121,7 @@ var tech_stream = client.stream("statuses/filter",
 					var time = new Date(now + delayTime);
 					console.log({"event": "LIKE", "url": url,"time": time});
 					setTimeout(function () {
-						client.post("favorites/create", {id: status_id}, 
+						client.post("favorites/create", {id: status_id},
 							function(error, data, response) {
 								if (!error) console.log({"event":"LIKE "+status_id});
 								else console.error(error);
@@ -103,7 +129,7 @@ var tech_stream = client.stream("statuses/filter",
 					},delayTime);
 			}
 		});
-			
+
 			stream.on("error", function(error) {
 				console.error(error);
 			});
